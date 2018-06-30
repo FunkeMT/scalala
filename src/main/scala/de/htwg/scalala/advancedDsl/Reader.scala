009ff1acfc4911e429374f313757793b8307480c
Markus Funke <funkemarkus@me.com>
2018-06-30 18:47:29 +0200

    bringt parser elements in some logical order

--- src/main/scala/de/htwg/scalala/advancedDsl/Reader.scala
+++ src/main/scala/de/htwg/scalala/advancedDsl/Reader.scala
@@ -16,8 +16,15 @@ class Reader extends StandardTokenParsers {
     "at")
   lexical.delimiters += (",", "(", ")")
 
-  def song: Parser[Song] = rep(track | musician) ^^ {
+
+  def song: Parser[Song] = rep(musician | track) ^^ {
     s => new Song(s)
+  }
+
+
+
+  def musician: Parser[Musician] = ("musician" ~> ident) ~ ("instrument" ~> instrument) ~ ("plays" ~> (loopElements | noteElements)) ^^ {
+    case m ~ i ~ e => new Musician(m, i, e)
   }
 
   def track: Parser[Track] = "play" ~ opt(tempo) ~ musicVars ^^ {
@@ -25,12 +32,34 @@ class Reader extends StandardTokenParsers {
     case p ~ t ~ m => new Track(m, t.get)
   }
 
+
+
+  def instrument: Parser[Instrument] = ident ^^ {
+    case i => new Instrument(i)
+  }
+
+  def noteElements: Parser[NoteElements] = repsep(noteElement, ",") ^^ {
+    case e => new NoteElements(e)
+  }
+
+  def loopElements: Parser[LoopElement] = ("loop" ~> "(") ~> noteElements <~ ")" ^^ {
+    case l => new LoopElement(l)
+  }
+
+
+
   def tempo: Parser[Int] = "with" ~> "tempo" ~> numericLit ^^ {
     n => n.toInt
   }
 
   def musicVars: Parser[List[MusicVar]] = repsep(musicVar, ",") ^^ {
     m => m
+  }
+
+
+
+  def noteElement: Parser[NoteElement] = chord | note ^^ {
+    e => e
   }
 
   def musicVar: Parser[MusicVar] = ident ~ opt(playAt) ^^ {
@@ -42,34 +71,19 @@ class Reader extends StandardTokenParsers {
     n => n.toInt
   }
 
-  def musician: Parser[Musician] = ("musician" ~> ident) ~ ("instrument" ~> instrument) ~ ("plays" ~> (loopElements | noteElements)) ^^ {
-    case m ~ i ~ e => new Musician(m, i, e)
-  }
 
-  def instrument: Parser[Instrument] = ident ^^ {
-    case i => new Instrument(i)
-  }
 
-  def noteElements: Parser[NoteElements] = repsep(noteElement, ",") ^^ {
-    case e => new NoteElements(e)
-  }
-
-  def noteElement: Parser[NoteElement] = chord | note ^^ {
-    e => e
-  }
-
-  def loopElements: Parser[LoopElement] = ("loop" ~> "(") ~> noteElements <~ ")" ^^ {
-    case l => new LoopElement(l)
+  def note: Parser[Note] = ident ^^ {
+    case i => new Note(i)
   }
 
   def chord: Parser[Chord] = ("chord" ~> "(") ~> repsep(note, ",") <~ ")" ^^ {
     case n => new Chord(n)
   }
 
-  def note: Parser[Note] = ident ^^ {
-    case i => new Note(i)
-  }
 
+
+  
   def parseAll[T](p: Parser[T], in: String): ParseResult[T] = {
     phrase(p)(new lexical.Scanner(in))
   }
